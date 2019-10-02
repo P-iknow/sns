1d789afaba194712f982cf5b04e30f1f6354ff56
P-iknow <apricotsoul@gmail.com>
2019-10-02 14:06:44 +0900

    refact: _app.js 생성하여 layout 분리
    - 공통 레이아웃을 _app.js 로 분리
    
    Closes #1

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,4 @@
 import React, { useState, useCallback } from 'react';
-import AppLayout from '../components/AppLayout';
-import Head from 'next/head';
 import { Form, Input, Checkbox, Button } from 'antd';
 import { useInput } from '../hooks';
 
@@ -41,70 +39,61 @@ const Signup = () => {
 
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"
-        />
-      </Head>
-      <AppLayout>
-        <Form onSubmit={onSubmit}>
-          <div>
-            <label htmlFor="user-id">아이디</label>
-            <br />
-            <Input name="user-id" required value={id} onChange={onChangeID} />
-          </div>
-          <div>
-            <label htmlFor="user-nick">닉네임</label>
-            <br />
-            <Input
-              name="user-nick"
-              required
-              value={nick}
-              onChange={onChangeNick}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password">비밀번호</label>
-            <br />
-            <Input
-              name="user-password"
-              type="password"
-              required
-              value={password}
-              onChange={onChangePassword}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password-check">비밀번호 확인</label>
-            <br />
-            <Input
-              name="user-password-check"
-              type="password"
-              required
-              value={passwordCheck}
-              onChange={onChangePasswordCheck}
-            />
-            {passwordError && (
-              <div style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</div>
-            )}
-          </div>
-          <div>
-            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
-              해당 사이트의 약관에 동의합니다.
-            </Checkbox>
-            {termError && (
-              <div style={{ color: 'red' }}>약관에 동의하셔야 합니다.</div>
-            )}
-          </div>
-          <div>
-            <Button type="primary" htmlType="submit">
-              가입하기
-            </Button>
-          </div>
-        </Form>
-      </AppLayout>
+      <Form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <Input name="user-id" required value={id} onChange={onChangeID} />
+        </div>
+        <div>
+          <label htmlFor="user-nick">닉네임</label>
+          <br />
+          <Input
+            name="user-nick"
+            required
+            value={nick}
+            onChange={onChangeNick}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">비밀번호</label>
+          <br />
+          <Input
+            name="user-password"
+            type="password"
+            required
+            value={password}
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password-check">비밀번호 확인</label>
+          <br />
+          <Input
+            name="user-password-check"
+            type="password"
+            required
+            value={passwordCheck}
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && (
+            <div style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</div>
+          )}
+        </div>
+        <div>
+          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+            해당 사이트의 약관에 동의합니다.
+          </Checkbox>
+          {termError && (
+            <div style={{ color: 'red' }}>약관에 동의하셔야 합니다.</div>
+          )}
+        </div>
+        <div>
+          <Button type="primary" htmlType="submit">
+            가입하기
+          </Button>
+        </div>
+      </Form>
     </>
   );
 };

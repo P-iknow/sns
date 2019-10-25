1c98c909ea46571f111004ba340f1d79c10fb211
P-iknow <apricotsoul@gmail.com>
2019-10-25 18:22:47 +0900

    feat: redux devtool 사용

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
+import withRedux from 'next-redux-wrapper';
 import AppLayout from '../components/AppLayout';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../reducers';
 
-const NodeBird = ({ Component }) => {
+const NodeBird = ({ Component, store }) => {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>NodeBird</title>
         <link
@@ -15,8 +21,25 @@ const NodeBird = ({ Component }) => {
       <AppLayout>
         <Component />
       </AppLayout>
-    </>
+    </Provider>
   );
 };
 
-export default NodeBird;
+NodeBird.propTypes = {
+  Component: PropTypes.elementType,
+  store: PropTypes.object,
+};
+
+export default withRedux((initialState, options) => {
+  const middlewares = [];
+  const enhancer = compose(
+    applyMiddleware(...middlewares),
+    !options.isServer &&
+      typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  );
+  const store = createStore(rootReducer, initialState, enhancer);
+  // store 커스터마이징 예정
+  return store;
+})(NodeBird);

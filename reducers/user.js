cfd5520a9cdae31d4053aecb0e574bdc1d3707e9
P-iknow <apricotsoul@gmail.com>
2019-10-25 15:46:02 +0900

    feat: user reducer

---
+++ reducers/user.js
@@ -0,0 +1,39 @@
+const initialState = {
+  isLoggedIn: false,
+  user: {},
+};
+
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+const loginAction = {
+  type: LOGIN,
+  data: {
+    nickname: 'piknow',
+  },
+};
+
+const logoutAction = {
+  type: LOGOUT,
+};
+
+const reducer = (state, initialState, action) => {
+  switch (action.type) {
+    case LOGIN: {
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.data,
+      };
+    }
+    case LOGOUT: {
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      };
+    }
+    default:
+      return state;
+  }
+};

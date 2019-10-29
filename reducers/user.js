32cfb00eb04316880a50b51ffeb555756926d247
P-iknow <apricotsoul@gmail.com>
2019-10-29 11:39:22 +0900

    feat: user reducer 상태 정리

--- reducers/user.js
+++ reducers/user.js
@@ -6,10 +6,29 @@ const dummyUser = {
 };
 
 const initialState = {
-  isLoggedIn: false,
-  user: null,
-  signUpData: {},
+  isLoggedIn: false, // 로그인 여부
+  isLoggingOut: false, // 로그아웃 시도중
+  isLoggingIn: false, // 로그인 시도중
+  LogInErrorReason: '', // 로그인 실패 사유
+  signedUp: false, // 회원가입 성공
+  isSigningUp: false, // 회원가입 시도중
+  signUpErrorReason: '', // 회원가입 실패 사유
+  me: null, // 내 정보
+  followingList: [], // 팔로잉 리스트
+  followerList: [], // 팔로워 리스트
+  userInfo: null, // 남의 정보
 };
+
+export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 
 const LOGIN = 'LOGIN';
 const LOGOUT = 'LOGOUT';
@@ -35,24 +54,63 @@ export const signUpAction = data => {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case LOGIN: {
+    case LOGIN_REQUEST: {
       return {
         ...state,
+        isLoggingIn: true,
+        LogInErrorReason: '',
+      };
+    }
+    case LOGIN_SUCCESS: {
+      return {
+        ...state,
+        isLoggingIn: false,
         isLoggedIn: true,
-        user: dummyUser,
+        me: dummyUser,
       };
     }
-    case LOGOUT: {
+    case LOGIN_FAILURE: {
       return {
         ...state,
+        isLoggingIn: false,
         isLoggedIn: false,
-        user: null,
+        LogInErrorReason: action.error,
+        me: null,
       };
     }
-    case SIGNUP: {
+    case LOGOUT_REQUEST: {
       return {
         ...state,
-        signUpData: action.data,
+        isLoggingOut: true,
+      };
+    }
+    case LOGOUT_SUCCESS: {
+      return {
+        ...state,
+        isLoggingOut: false,
+        me: null,
+      };
+    }
+    case SIGNUP_REQUEST: {
+      return {
+        ...state,
+        isSigningUp: true,
+        isSignedUp: false,
+        signUpErrorReason: '',
+      };
+    }
+    case SIGNUP_SUCCESS: {
+      return {
+        ...state,
+        isSigningUp: false,
+        isSignedUp: true,
+      };
+    }
+    case SIGNUP_FAILURE: {
+      return {
+        ...state,
+        isSigningUp: false,
+        signUpErrorReason: action.error,
       };
     }
     default:

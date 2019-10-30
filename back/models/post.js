90f11d3b152fa2a4cb1d549df6ddb434e6f14ca0
P-iknow <apricotsoul@gmail.com>
2019-10-30 20:39:15 +0900

    feat: db models/image

--- back/models/post.js
+++ back/models/post.js
@@ -8,7 +8,7 @@ module.export = (sequelize, DataTypes) => {
       }
     },
     {
-      charset: 'utf8mb4',
+      charset: 'utf8mb4', // 한글 + 이모티콘 까지됨
       collate: 'utf8mb4_general_ci'
     }
   );

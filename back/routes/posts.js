ea1157fddc724f31d4498e28f02ff85394566010
P-iknow <apricotsoul@gmail.com>
2019-11-08 22:50:04 +0900

    fix: req, res 인자 순서 수정
    - GET api/posts 라우터 콜백함수에서 req 와 res 인자의 순서를 바꿔서,
      res.json 이 undefined 된 오류가 있었으나 변경함

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -3,7 +3,7 @@ const db = require('../models');
 
 const router = express.Router();
 
-router.get('/', async (res, req, next) => {
+router.get('/', async (req, res, next) => {
   // GET /api/posts
   try {
     const posts = await db.Post.findAll({
@@ -12,7 +12,8 @@ router.get('/', async (res, req, next) => {
           model: db.User,
           attributes: ['id', 'nickname']
         }
-      ]
+      ],
+      order: [['createdAt', 'DESC']] // DESC는 내림차순, ASC는 오름차순
     });
     res.json(posts);
   } catch (e) {

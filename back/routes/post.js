1b64088330885504bfeb2b5224e41bc64df92c85
P-iknow <apricotsoul@gmail.com>
2019-11-08 22:26:08 +0900

    feat: POST  api/post
    - 글 작성하기 구현

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,8 +1,49 @@
 const express = require('express');
+const db = require('../models');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {});
+router.post('/', async (req, res, next) => {
+  // POST /api/post
+  try {
+    const newPost = await db.Post.create({
+      content: req.body.content,
+      UserId: req.user.id
+    });
+    const hashtags = req.body.content.match(/#[^\s]/g);
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map(tag => {
+          const processedHashtag = tag.slice(1).toLowerCase();
+          return db.Hashtag.findOrCreate({
+            where: { name: processedHashtag }
+          });
+        })
+      );
+      await newPost.addHashtags(result.map(hashtag => hashtag[0]));
+    }
+
+    // 이런 방법이 있고
+    // const User = await newPost.getUser()l 여기서 유저는 작성자 한명뿐
+    // newPost.User = User;
+    // res.json(newPost);
+
+    const fullPost = await db.Post.findOne({
+      where: { id: newPost.id },
+      include: [
+        {
+          model: db.User,
+          attributes: ['id', 'nickname', 'userId']
+        }
+      ]
+    });
+
+    res.json(fullPost);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
 
 router.post('/imeage', (req, res) => {});
 

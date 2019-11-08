94994a473dc611402c3e3bc62f47eb55b4ca7903
P-iknow <apricotsoul@gmail.com>
2019-11-08 22:31:56 +0900

    feat: GET api/posts

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const db = require('../models');
 
 const router = express.Router();
 
-router.get('/', (res, req) => {});
+router.get('/', async (res, req, next) => {
+  // GET /api/posts
+  try {
+    const posts = await db.Post.findAll({
+      include: [
+        {
+          model: db.User,
+          attributes: ['id', 'nickname']
+        }
+      ]
+    });
+    res.json(posts);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
 
 module.exports = router;
